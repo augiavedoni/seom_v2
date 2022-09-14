57e8fa847a90a9b4287c5b078d62a4760766d3fa
Augusto Giavedoni <58192738+augiavedoni@users.noreply.github.com>
2022-09-13 22:44:51 -0300

    Remove unused import

--- lib/application/vehicles/vehicle_watcher/vehicle_watcher_bloc.dart
+++ lib/application/vehicles/vehicle_watcher/vehicle_watcher_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:kt_dart/collection.dart';
 
