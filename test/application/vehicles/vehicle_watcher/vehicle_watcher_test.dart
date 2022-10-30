6f39f08a5a235af32459e8f8669ef7effe989fbd
Augusto Giavedoni <58192738+augiavedoni@users.noreply.github.com>
2022-10-30 18:12:52 -0300

    Implement VehicleActorBloc test cases

--- test/application/vehicles/vehicle_watcher/vehicle_watcher_test.dart
+++ test/application/vehicles/vehicle_watcher/vehicle_watcher_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:kt_dart/kt.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:seom_v2/domain/vehicles/entities/vehicle.dart';
 import 'package:seom_v2/domain/vehicles/vehicle_failure.dart';
 import 'package:seom_v2/infrastructure/auth/dto/seom_user_dto.dart';
 import 'package:seom_v2/infrastructure/core/http/dto/http_error_dto.dart';
@@ -39,20 +37,8 @@ main() {
   final userDataSource = MockUserDataSource();
 
   group(
-    'VehicleRepository success cases tests',
+    'VehicleWatcherBloc success case test',
     () {
-      const ten = 10.0;
-      final position = Position(
-        accuracy: ten,
-        altitude: ten,
-        heading: ten,
-        latitude: ten,
-        longitude: ten,
-        speed: ten,
-        speedAccuracy: ten,
-        timestamp: DateTime.now(),
-        isMocked: true,
-      );
       final seomClientMock = SeomClientMock(
         onGet: <T>() async => HTTPResponseType.ok(List.empty() as T),
         onPatch: <T>() async => HTTPResponseType.ok(vehicle.toJson() as T),
@@ -73,40 +59,11 @@ main() {
           const KtList.empty(),
         );
       });
-
-      test(
-        'Should receive a vehicle when we call park',
-        () async {
-          final result = await vehicleRepository.park(
-            vehicle.toDomain(),
-            position,
-          );
-
-          expect(result.isRight(), true);
-          expect(
-            result.getOrElse(() => vehicle.toDomain()),
-            isA<Vehicle>(),
-          );
-        },
-      );
-
-      test(
-        'Should receive a vehicle when we call unpark',
-        () async {
-          final result = await vehicleRepository.unpark(vehicle.toDomain());
-
-          expect(result.isRight(), true);
-          expect(
-            result.getOrElse(() => vehicle.toDomain()),
-            isA<Vehicle>(),
-          );
-        },
-      );
     },
   );
 
   group(
-    'VehicleRepository VehicleFailureUnexpected error case tests',
+    'VehicleWatcherBloc VehicleFailureUnexpected error case tests',
     () {
       const error = HTTPErrorDto(
         error: 'test',
@@ -137,153 +94,6 @@ main() {
               (_) => null,
             ),
             isA<VehicleFailureUnexpected>(),
-          );
-        },
-      );
-
-      test(
-        'Should receive VehicleFailureUnexpected when we call park',
-        () async {
-          when(userDataSource.user).thenReturn(seomUser.toDomain());
-
-          final result = await vehicleRepository.park(vehicle.toDomain(), null);
-
-          expect(result.isLeft(), true);
-          expect(
-            result.fold(
-              (failure) => failure,
-              (_) => null,
-            ),
-            isA<VehicleFailureUnexpected>(),
-          );
-        },
-      );
-
-      test(
-        'Should receive VehicleFailureUnexpected when we call unpark',
-        () async {
-          when(userDataSource.user).thenReturn(seomUser.toDomain());
-
-          final result = await vehicleRepository.unpark(vehicle.toDomain());
-
-          expect(result.isLeft(), true);
-          expect(
-            result.fold(
-              (failure) => failure,
-              (_) => null,
-            ),
-            isA<VehicleFailureUnexpected>(),
-          );
-        },
-      );
-    },
-  );
-
-  group(
-    'VehicleRepository VehicleFailureAlreadyParked error case test',
-    () {
-      const error = HTTPErrorDto(
-        error: 'vehicle-already-parked',
-        message: 'test',
-        path: 'test',
-        status: errorStatusCode,
-      );
-      final seomClientMock = SeomClientMock(
-        onPatch: <T>() async => HTTPResponseType.error(error, errorStatusCode),
-      );
-      final vehicleRepository = VehicleRepository(
-        seomClientMock,
-        userDataSource,
-      );
-
-      test(
-        'Should receive VehicleFailureAlreadyParked when we call park',
-        () async {
-          when(userDataSource.user).thenReturn(seomUser.toDomain());
-
-          final result = await vehicleRepository.park(vehicle.toDomain(), null);
-
-          expect(result.isLeft(), true);
-          expect(
-            result.fold(
-              (failure) => failure,
-              (_) => null,
-            ),
-            isA<VehicleFailureAlreadyParked>(),
-          );
-        },
-      );
-    },
-  );
-
-  group(
-    'VehicleRepository VehicleFailureVehicleNotFound error case test',
-    () {
-      const error = HTTPErrorDto(
-        error: 'vehicle-not-found',
-        message: 'test',
-        path: 'test',
-        status: errorStatusCode,
-      );
-      final seomClientMock = SeomClientMock(
-        onPatch: <T>() async => HTTPResponseType.error(error, errorStatusCode),
-      );
-      final vehicleRepository = VehicleRepository(
-        seomClientMock,
-        userDataSource,
-      );
-
-      test(
-        'Should receive VehicleFailureVehicleNotFound when we call park',
-        () async {
-          when(userDataSource.user).thenReturn(seomUser.toDomain());
-
-          final result = await vehicleRepository.park(vehicle.toDomain(), null);
-
-          expect(result.isLeft(), true);
-          expect(
-            result.fold(
-              (failure) => failure,
-              (_) => null,
-            ),
-            isA<VehicleFailureVehicleNotFound>(),
-          );
-        },
-      );
-    },
-  );
-
-  group(
-    'VehicleRepository VehicleFailureParkingTicketNotFound error case test',
-    () {
-      const error = HTTPErrorDto(
-        error: 'parking-ticket-not-found',
-        message: 'test',
-        path: 'test',
-        status: errorStatusCode,
-      );
-      final seomClientMock = SeomClientMock(
-        onPatch: <T>() async => HTTPResponseType.error(error, errorStatusCode),
-      );
-      final vehicleRepository = VehicleRepository(
-        seomClientMock,
-        userDataSource,
-      );
-
-      test(
-        'Should receive VehicleFailureParkingTicketNotFound when we call unpark',
-        () async {
-          when(userDataSource.user).thenReturn(seomUser.toDomain());
-
-          final result = await vehicleRepository.unpark(vehicle.toDomain());
-
-          expect(result.isLeft(), true);
-          expect(
-            result.fold(
-              (failure) => failure,
-              (_) => null,
-            ),
-            isA<VehicleFailureParkingTicketNotFound>(),
           );
         },
       );
